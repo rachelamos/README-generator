1707c07a83a444072f14638568311c9b1f496703
Rachel Amos <rachelamos35@gmail.com>
2021-02-28 11:48:00 -0600

    adding all required questions to JS file

--- index.js
+++ index.js
@@ -4,18 +4,60 @@ const fs = require('fs');
 inquirer
     .prompt([
         {
-            // project title
-            // description
-            // installation instructions
-            // usage information
-            // confirmation guidelines
-            // test instructions
-            // choose license
-            // GitHub username
-            // email address
-
-
-        }
+            type: 'input',
+            name: 'name',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please give a description of your project.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What is needed to use this application?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How would a user utilize this application?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Who contributed to your project?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'How should a user test this application?',
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Choose a license for your project',
+            choices: ['MIT', 'GNU GPLv3', 'Babel'],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address.',
+          },
     ])
+    .then(answers => {
 
-    
+    })
+    // .catch(error => {
+    //     if(error.isTtyError) {
+    //         else{
+
+    //         }
+    //     }
+    // });
+
