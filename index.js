166bc0bd1b57e0d871f44168826233d49d524e42
Rachel Amos <rachelamos35@gmail.com>
2021-02-28 11:00:11 -0600

    updating gitignore

---
+++ index.js
@@ -0,0 +1,19 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            // project title
+            // description
+            // installation instructions
+            // usage information
+            // confirmation guidelines
+            // test instructions
+            // choose license
+            // GitHub username
+            // email address
+            
+
+        }
+    ])
