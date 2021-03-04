c4bb6a1f34660a45783c4710e37cab7fe39a1316
Rachel Amos <rachelamos35@gmail.com>
2021-03-04 16:06:21 -0600

    moving function generateMarkdown to generateMarkdown.js file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,20 @@
 let licenseBadge;
-let licenceLink;
+let licenseLink;
 let licenseSection;
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    switch (license){
-        case "MIT" :
+    switch (license) {
+        case "MIT":
             licenseBadge = "";
             break;
-        case "GNU GPLv3" :
+        case "GNU GPLv3":
             licenseBadge = "";
             break;
-        case "Babel" :
+        case "Babel":
             licenseBadge = "";
             break;
-        default :
+        default:
             licenseBadge = "";
     }
     renderLicenseBadge(license, licenseBadge);
@@ -23,17 +23,17 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    switch (license){
-        case "MIT" :
+    switch (license) {
+        case "MIT":
             licenseLink = "";
             break;
-        case "GNU GPLv3" :
+        case "GNU GPLv3":
             licenseLink = "";
             break;
-        case "Babel" :
+        case "Babel":
             licenseLink = "";
             break;
-        default :
+        default:
             licenseLink = "";
     }
     renderLicenseLink(license, licenseLink);
@@ -42,17 +42,17 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    switch (license){
-        case "MIT" :
+    switch (license) {
+        case "MIT":
             licenseSection = "";
             break;
-        case "GNU GPLv3" :
+        case "GNU GPLv3":
             licenseSection = "";
             break;
-        case "Babel" :
+        case "Babel":
             licenseSection = "";
             break;
-        default :
+        default:
             licenseSection = "";
     }
     renderLicenseSection(license, licenseSection);
@@ -60,9 +60,43 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `
+    # ${data.title}
 
-`;
+    ## Table of Contents
+
+    1. [Description](#description)
+    2. [Installation](#installation-instructions)
+    3. [Usage](#usage)
+    4. [Contributing](#contributers)
+    5. [Tests](#test)
+        
+    ### Description
+    ${data.description}
+
+    ### Installation Instructions
+    ${data.installation}
+
+    ### Usage
+    ${data.usage}
+
+    ### Contributers
+    ${data.contribution}
+
+    ### Test
+    ${data.test}
+
+    ### Questions
+    If you have any other questions, you can reach me:
+    - via email: ${data.email}
+    - via GitHub: github.com/${data.github}
+
+    ### License & Copyright
+    This project is covered under the [${data.license}](LICENSE).
+
+    © ${data.contribution}`;
+
+
 }
 
 module.exports = generateMarkdown;
